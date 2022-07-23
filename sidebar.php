b57c1f8ca0d3c6c14326aa345b805e76e51d71fa
John Doe <johndoe@example.com>
2022-07-24 00:08:18 +0900

    0724

---
+++ sidebar.php
@@ -0,0 +1,83 @@
+<aside class="blog-side">
+          <div class="blog-side__about">
+            <div class="blog-side__about-title">
+              <h3>クリニックの紹介</h3>
+            </div>
+            <figure class="blog-side__about-img">
+              <img src="<?php echo esc_url(get_theme_file_uri('/src/images/blog/sidebar-img_1.jpg')); ?>" alt="">
+            </figure>
+            <div class="blog-side__about-text">
+              <p>みなみ歯科クリニック</p>
+              <p>お子様からご高齢の方まで、快適な空間で治療が受けられる場を作り、地域医療に貢献しきたいと考えております。</p>
+            </div>
+            <div class="blog-side__about-link">
+              <a href="">当院について</a>
+            </div>
+          </div>
+          <div class="blog-side__post">
+            <div class="blog-side__post-title">
+              <h3>新着記事</h3>
+            </div>
+            <div class="blog-main__items">
+              <a class="blog-main__item">
+                <figure class="blog-main__img">
+                  <img src="./src/images/top/blog.jpg" alt="ブログカード1">
+                </figure>
+                <div class="blog-main__body">
+                  <div class="blog-main__category">お知らせ</div>
+                  <p class="blog-main__title">記事のタイトルが入ります。記事のタイトルが入ります…</p>
+                  <time class="blog-main__date" datetime="2020-02-14">2020.02.14</time>
+                </div>
+              </a>
+              <a class="blog-main__item">
+                <figure class="blog-main__img">
+                  <img src="./src/images/top/blog.jpg" alt="ブログカード1">
+                </figure>
+                <div class="blog-main__body">
+                  <div class="blog-main__category">お知らせ</div>
+                  <p class="blog-main__title">記事のタイトルが入ります。記事のタイトルが入ります…</p>
+                  <time class="blog-main__date" datetime="2020-02-14">2020.02.14</time>
+                </div>
+              </a>
+              <a class="blog-main__item">
+                <figure class="blog-main__img">
+                  <img src="./src/images/top/blog.jpg" alt="ブログカード1">
+                </figure>
+                <div class="blog-main__body">
+                  <div class="blog-main__category">お知らせ</div>
+                  <p class="blog-main__title">記事のタイトルが入ります。記事のタイトルが入ります…</p>
+                  <time class="blog-main__date" datetime="2020-02-14">2020.02.14</time>
+                </div>
+              </a>
+              <a class="blog-main__item">
+                <figure class="blog-main__img">
+                  <img src="./src/images/top/blog.jpg" alt="ブログカード1">
+                </figure>
+                <div class="blog-main__body">
+                  <div class="blog-main__category">お知らせ</div>
+                  <p class="blog-main__title">記事のタイトルが入ります。記事のタイトルが入ります…</p>
+                  <time class="blog-main__date" datetime="2020-02-14">2020.02.14</time>
+                </div>
+              </a>
+              <a class="blog-main__item">
+                <figure class="blog-main__img">
+                  <img src="./src/images/top/blog.jpg" alt="ブログカード1">
+                </figure>
+                <div class="blog-main__body">
+                  <div class="blog-main__category">お知らせ</div>
+                  <p class="blog-main__title">記事のタイトルが入ります。記事のタイトルが入ります…</p>
+                  <time class="blog-main__date" datetime="2020-02-14">2020.02.14</time>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="blog-side__category">
+            <h3 class="blog-side__category-title">カテゴリー</h3>
+            <ul class="blog-side__category-items">
+              <li class="blog-side__category-item"><a href="">テキスト</a></li>
+              <li class="blog-side__category-item"><a href="">テキスト</a></li>
+              <li class="blog-side__category-item"><a href="">テキスト</a></li>
+              <li class="blog-side__category-item"><a href="">テキスト</a></li>
+            </ul>
+          </div>
+      </aside>
