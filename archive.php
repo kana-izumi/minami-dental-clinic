3b91466f921ab5c544da5465185c24b72f2ed797
John Doe <johndoe@example.com>
2022-07-24 14:59:30 +0900

    0724

---
+++ archive.php
@@ -0,0 +1,144 @@
+<?php get_header(); ?>
+    <div class="mv-sub">
+      <div class="mv-sub__inner inner">
+        <div class="mv-sub__img u-mobile">
+          <img src="./src/images/blog/archive_top-sp.jpg" alt="スタッフブログ画像">
+        </div>
+        <div class="mv-sub__img u-desktop">
+          <img src="./src/images/blog/archive_top.jpg" alt="スタッフブログ画像">
+        </div>
+        <div class="mv-sub__heading">
+          <h2 class="mv-sub__title">スタッフブログ</h2>
+          <span class="mv-sub__lead">STAFF BLOG</span>
+        </div>
+        <div class="bread">
+          <ul class="bread__list">
+            <li class="bread__item bread__item-grey"><a href="#">ホーム</a></li>
+            <li class="bread__item"><a href="#">スタッフ紹介</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!-- /.mv-sub -->
+    <div class="blog blog-wrapper inner">
+      <article class="blog-main">
+        <div class="blog-main__inner">
+          <div class="blog-main__items">
+            <a class="blog-main__item">
+                <figure class="blog-main__img">
+                  <img src="./src/images/blog/blog.jpg" alt="ブログカード1">
+                </figure>
+                <div class="blog-main__body">
+                  <div class="blog-main__category">お知らせ</div>
+                  <p class="blog-main__title">記事のタイトルが入ります。記事のタイトルが入ります…</p>
+                  <time class="blog-main__date" datetime="2020-02-14">2020.02.14</time>
+                </div>
+            </a>
+            <a class="blog-main__item">
+                <figure class="blog-main__img">
+                  <img src="./src/images/blog/blog.jpg" alt="ブログカード1">
+                </figure>
+                <div class="blog-main__body">
+                  <div class="blog-main__category">お知らせ</div>
+                  <p class="blog-main__title">記事のタイトルが入ります。記事のタイトルが入ります…</p>
+                  <time class="blog-main__date" datetime="2020-02-14">2020.02.14</time>
+                </div>
+            </a>
+            <a class="blog-main__item">
+                <figure class="blog-main__img">
+                  <img src="./src/images/blog/blog.jpg" alt="ブログカード1">
+                </figure>
+                <div class="blog-main__body">
+                  <div class="blog-main__category">お知らせ</div>
+                  <p class="blog-main__title">記事のタイトルが入ります。記事のタイトルが入ります…</p>
+                  <time class="blog-main__date" datetime="2020-02-14">2020.02.14</time>
+                </div>
+            </a>
+            <a class="blog-main__item">
+                <figure class="blog-main__img">
+                  <img src="./src/images/blog/blog.jpg" alt="ブログカード1">
+                </figure>
+                <div class="blog-main__body">
+                  <div class="blog-main__category">お知らせ</div>
+                  <p class="blog-main__title">記事のタイトルが入ります。記事のタイトルが入ります…</p>
+                  <time class="blog-main__date" datetime="2020-02-14">2020.02.14</time>
+                </div>
+            </a>
+            <a class="blog-main__item">
+                <figure class="blog-main__img">
+                  <img src="./src/images/blog/blog.jpg" alt="ブログカード1">
+                </figure>
+                <div class="blog-main__body">
+                  <div class="blog-main__category">お知らせ</div>
+                  <p class="blog-main__title">記事のタイトルが入ります。記事のタイトルが入ります…</p>
+                  <time class="blog-main__date" datetime="2020-02-14">2020.02.14</time>
+                </div>
+            </a>
+            <a class="blog-main__item">
+                <figure class="blog-main__img">
+                  <img src="./src/images/blog/blog.jpg" alt="ブログカード1">
+                </figure>
+                <div class="blog-main__body">
+                  <div class="blog-main__category">お知らせ</div>
+                  <p class="blog-main__title">記事のタイトルが入ります。記事のタイトルが入ります…</p>
+                  <time class="blog-main__date" datetime="2020-02-14">2020.02.14</time>
+                </div>
+              </a>
+            <a class="blog-main__item">
+                <figure class="blog-main__img">
+                  <img src="./src/images/blog/blog.jpg" alt="ブログカード1">
+                </figure>
+                <div class="blog-main__body">
+                  <div class="blog-main__category">お知らせ</div>
+                  <p class="blog-main__title">記事のタイトルが入ります。記事のタイトルが入ります…</p>
+                  <time class="blog-main__date" datetime="2020-02-14">2020.02.14</time>
+                </div>
+              </a>
+            <a class="blog-main__item">
+                <figure class="blog-main__img">
+                  <img src="./src/images/blog/blog.jpg" alt="ブログカード1">
+                </figure>
+                <div class="blog-main__body">
+                  <div class="blog-main__category">お知らせ</div>
+                  <p class="blog-main__title">記事のタイトルが入ります。記事のタイトルが入ります…</p>
+                  <time class="blog-main__date" datetime="2020-02-14">2020.02.14</time>
+                </div>
+              </a>
+            <a class="blog-main__item">
+                <figure class="blog-main__img">
+                  <img src="./src/images/blog/blog.jpg" alt="ブログカード1">
+                </figure>
+                <div class="blog-main__body">
+                  <div class="blog-main__category">お知らせ</div>
+                  <p class="blog-main__title">記事のタイトルが入ります。記事のタイトルが入ります…</p>
+                  <time class="blog-main__date" datetime="2020-02-14">2020.02.14</time>
+                </div>
+              </a>
+            <a class="blog-main__item">
+                <figure class="blog-main__img">
+                  <img src="./src/images/blog/blog.jpg" alt="ブログカード1">
+                </figure>
+                <div class="blog-main__body">
+                  <div class="blog-main__category">お知らせ</div>
+                  <p class="blog-main__title">記事のタイトルが入ります。記事のタイトルが入ります…</p>
+                  <time class="blog-main__date" datetime="2020-02-14">2020.02.14</time>
+                </div>
+              </a>
+            <div class="blog-main__pager">
+              <ul class="blog-main__pagination">
+                  <li class="blog-main__pre"><a href="#"><span>前へ</span></a></li>
+                  <li class="blog-main__all"><a href="#" class="active"><span>1</span></a></li>
+                  <li class="blog-main__all"><a href="#"><span>2</span></a></li>
+                  <li class="blog-main__all"><a href="#"><span>3</span></a></li>
+                  <li class="blog-main__all"><a href="#"><span>‥</span></a></li>
+                  <li class="blog-main__all"><a href="#"><span>20</span></a></li>
+                  <li class="blog-main__next"><a href="#"><span>次へ</span></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </article>
+<?php get_sidebar(); ?>
+    </div>
+    <!-- /.staff.blog -->
+<?php get_footer();?>
