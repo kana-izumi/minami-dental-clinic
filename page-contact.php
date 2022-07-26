93569c3ee8c128bc476ae388e74e78a3fca4253f
John Doe <johndoe@example.com>
2022-07-26 22:58:27 +0900

    0726

--- page-contact.php
+++ page-contact.php
@@ -29,36 +29,15 @@
           <p><span>※3営業日以内に当院からの返信がない場合には、お電話(TEL 03-1234-5678)にてお問い合わせ下さい。<span></p>
         </div>
         <div class="contact__wrapper">
-          <div class="section-header">
-            <h2 class="section-header__title">お問い合わせ<br>フォーム</h2>
-          </div>
-          <form class="contact-form" action="">
-            <dl class="contact-form__dl">
-                <div class="contact-form__row">
-                    <dt class="contact-form__label"><label for="your-name" class="is-required">お名前</label></dt>
-                    <dd class="contact-form__input"><input type="text" id="your-name" placeholder="山田 太郎"></dd>
-                </div>
-                <div class="contact-form__row">
-                    <dt class="contact-form__label"><label for="your-kana" class="is-required">フリガナ</label></dt>
-                    <dd class="contact-form__input"><input type="text" id="your-kana" placeholder="ヤマダ タロウ"></dd>
-                </div>
-                <div class="contact-form__row">
-                    <dt class="contact-form__label"><label for="your-tel">電話番号</label></dt>
-                    <dd class="contact-form__input"><input type="text" id="your-tel" placeholder="000-0000-0000"></dd>
-                </div>
-                <div class="contact-form__row">
-                    <dt class="contact-form__label"><label for="your-email">メールアドレス</label></dt>
-                    <dd class="contact-form__input"><input type="email" id="your-email" placeholder="sample@gmail.com"></dd>
-                </div>
-                <div class="contact-form__row">
-                    <dt class="contact-form__label"><label for="your-text">お問い合わせ内容</label></dt>
-                    <dd class="contact-form__text"><textarea name="" id="your-tex" placeholder="ご自由にご記入ください。"></textarea></dd>
-                </div>
-                <div class="contact-form__btn">
-                    <input type="submit" value="送　信">
-                </div>
-            </dl>
-        </form>
+          <?php if(have_posts()): ?>
+            <div class="contact-header">
+              <h2 class="contact-header__title"><?php the_title(); ?><br>フォーム</h2>
+            </div>
+            <?php while(have_posts()) : the_post(); ?>
+              <?php the_content(); ?>
+              <?php echo do_shortcode('[contact-form-7 id="36" title="お問い合わせ"]')?>
+            <?php endwhile;?>
+          <?php endif; ?>
         </div>
       </div>
     </section>
